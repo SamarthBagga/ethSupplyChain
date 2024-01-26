61c44ff974327e692a37c950d211235b67f79aa6
SamarthBagga <samarthbagga@gmail.com>
2024-01-26 23:30:44 +0530

    contract

---
+++ web3/contracts/SupplyChain.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+/**@title Check Supply Chain Authenticity
+ * @author Rahul Arora
+ * @author Samarth Bagga
+ * @notice This contract keeps a record of all the nodes in a supply chain thereby maintaining authenticity
+ * @dev idk
+ */
+
+contract SupplyChain {
+    
+}
