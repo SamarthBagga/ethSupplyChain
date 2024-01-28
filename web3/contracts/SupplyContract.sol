5f385deebd63631cd812053d1d075630fb2370ca
SamarthBagga <samarthbagga@gmail.com>
2024-01-28 18:59:02 +0530

    contracts

--- web3/contracts/SupplyContract.sol
+++ web3/contracts/SupplyContract.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.9;
 /**@title Check Supply Chain Authenticity
  * @author Rahul Arora
  * @author Samarth Bagga
- * @notice This contract holds all the supplyContracts created
+ * @notice This contract keeps a record of all the nodes in a supply chain thereby maintaining authenticity
  * @dev idk
  */
 
-contract SupplyChain {
+contract SupplyContract {
     
 }
--- web3/contracts/SupplyToken.sol
+++ web3/contracts/SupplyToken.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.9;
 /**@title Check Supply Chain Authenticity
  * @author Rahul Arora
  * @author Samarth Bagga
- * @notice This contract holds all the supplyContracts created
+ * @notice This is the native token for the supply chain
  * @dev idk
  */
 
-contract SupplyChain {
+contract SupplyToken {
     
 }
