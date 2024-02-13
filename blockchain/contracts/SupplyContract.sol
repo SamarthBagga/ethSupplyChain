d57e86f91882c314e3d3195cd8827e8dc8960486
SamarthBagga <samarthbagga@gmail.com>
2024-02-13 23:52:52 +0530

    supplyContract2

--- blockchain/contracts/SupplyContract.sol
+++ blockchain/contracts/SupplyContract.sol
@@ -6,105 +6,119 @@
  * @notice This is the native token for the supply chain
  * @dev idk
  */
-pragma solidity ^0.8.9;
+
+ pragma solidity ^0.8.9;
+
 interface IERC20 {
     function balanceOf(address account) external view returns (uint);
 }
-error FundMe_NotOwner();
-contract SupplyContract {
-    bool isOpen = true;
+
+ contract SupplyContract2 {
     address private immutable i_owner;
-    Node[] public supplyChainMap;
-    mapping(address => uint256) balances;
-    uint256 total = 0;
-   
-    constructor() {
-    i_owner = msg.sender;
-}
-    modifier onlyOwner() {
-        // require(msg.sender == i_owner);
-        if (msg.sender != i_owner) revert FundMe_NotOwner();
-        _;
-    }
+    Order[] public activeOrders;
+    mapping (uint256 => Order) activeOrdersMap;
+    Order[] public pastOrders;
+    mapping (uint256 => Order) pastOrdersMap;
+    Node[] public chain;
+    mapping (uint256 => Node) chainMap;
+
     struct Node {
+        uint256 nodeID;
         string name;
         string typeOfNode; // whether it is a manufacturer supplier etc
-        string location;
-        bool reached; // if reached is true but verified is false that means that it is still in progress on the location
-        bool verified;
         address add;
-        uint256 payment;
+        uint256[] ordersPresent; // only the id of the orders, if we want the orders present at the node then we would have to loop through the activeOrders and match the id from this array to get the orders present at the location
+        string location;
+        uint256 profitShare; // Must be in points as the cost of the order will be divided
+        // uint256 payment;
     }
-    function getTokenBalance(address tokenAddress, address accountAddress) external view returns (uint) {
-        IERC20 token = IERC20(tokenAddress);
-        return token.balanceOf(accountAddress);
+    struct Order {
+        string name;
+        uint256 orderID;
+        uint256 currLocation;
+        uint256 finalLocation;
+        Node[] pathTillNow;
+        uint256 cost;
+        bool isDone;
     }
-    function getTokenBalanceInt(address tokenAddress, address accountAddress) internal  view returns (uint) {
-        IERC20 token = IERC20(tokenAddress);
-        return token.balanceOf(accountAddress);
-    }
-    function ifOpen() external view returns(bool) {
-        return isOpen;
-    }
-    function closeChain() internal  {
-        isOpen = false;
-        for (uint i = 0; i < supplyChainMap.length; i++) {
-            uint256 pay = supplyChainMap[i].payment;
-            payable(supplyChainMap[i].add).transfer(pay);
+        constructor() {
+    i_owner = msg.sender;
         }
-    }
-    function createchain(Node[] memory items) external onlyOwner {
-        for (uint i = 0; i < items.length; i++) {
-            supplyChainMap[i] = items[i];
-            total += supplyChainMap[i].payment;
-        }
-    }
-    function addNode(Node memory item) external onlyOwner {
-        supplyChainMap[supplyChainMap.length - 1] = item;
-        total += item.payment;
-    }
-    function returnChain() external view returns (Node[] memory) {
-        return supplyChainMap;
-    }
-    function resetChain() internal onlyOwner {
-        isOpen = true;
-        for (uint i = 0; i < supplyChainMap.length; i++) {
-            supplyChainMap[i].reached = false;
-            supplyChainMap[i].verified = false;
-        }
+    function createOrder(
+        string memory name,
+        uint256 orderID,
+        uint256 currLocation,
+        uint256 finalLocation,
+        Node[] memory pathTillNow,
+        uint256 cost,
+        bool isDone
+    ) public {
+        Order memory newOrder = Order({
+            name: name,
+            orderID: orderID,
+            currLocation: currLocation,
+            finalLocation: finalLocation,
+            pathTillNow: pathTillNow,
+            cost: cost,
+            isDone: isDone
+        });
+
+        // Store the new order in the mapping
+        activeOrders[activeOrders.length - 1] = newOrder;
+        activeOrdersMap[orderID] = newOrder;
     }
 
-    function setVerified(address add1) external  {
-        for (uint i = 0; i < supplyChainMap.length; i++) {
-            if (supplyChainMap[i].add == add1) {
-                supplyChainMap[i].verified = true;
-            }
-        }
-        uint256 bal = getTokenBalanceInt(0x4E7C2641D4d3B46D9db416302B32B617bBA294Cc, address(this));
-        if (bal == 0) {
-            closeChain();
-        }
+    function getActiveOrders() public view returns (Order[] memory) {
+        return activeOrders;
     }
-
-    function setReached(address add1) external  {
-        for (uint i = 0; i < supplyChainMap.length; i++) {
-            if (supplyChainMap[i].add == add1) {
-                supplyChainMap[i].reached = true;
-            }
+    function getPastOrders() public view returns (Order[] memory) {
+        return pastOrders;
+    }
+    function createChain(Node[] memory nodes) public view {
+        for(uint i = 0; i < nodes.length; i++) {
+            nodes[i] = chain[i];
+            chainMap[nodes[i].nodeID] = nodes[i]; 
         }
     }
-
-   
-   function deposit() external payable {
-     require(msg.value > 0, "Zero ether not allowed");
-     require(msg.value > total, "Payment should be greater than total");
-     balances[msg.sender] = balances[msg.sender] + msg.value;
-     resetChain();
-     
-   }
-
-    // Function to get the contract balance
-    function getContractBalance() external view returns (uint256) {
-        return address(this).balance;
+    function addNode(Node memory node, uint256 pos) public  {
+        require(pos < chain.length, "Position is invalid");
+        Node memory last;
+        if(pos == 1) {
+            last = chain[0];
+        for (uint i = 1; i < chain.length; i++) {
+            Node memory temp = chain[i];
+            chain[i] = last;
+            last = temp;
+        }
+        chain[0] = node;
+        return;
+        }
+        last = chain[pos - 1];
+        for (uint i = pos; i < chain.length; i++) {
+            Node memory temp = chain[i];
+            chain[i] = last;
+            last = temp;
+        }
+        chain[pos - 1] = node;
+        return;
     }
-}
+    function deleteNode(uint256 id) public {
+    for (uint i = 0; i < chain.length; i++) {
+        if (chain[i].nodeID == id) {
+            chain[i] = chain[chain.length - 1];
+            chain.pop();
+        }
+    }
+}  
+    function moveOrder(uint256 orderID) public {
+        for(uint i = 0; i < chain.length; i++) {
+            
+        } 
+    }
+    function sendMoney
+    
+
+
+
+
+ }
