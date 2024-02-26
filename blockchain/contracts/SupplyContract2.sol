ec7cdccb13fa84b7719c5808d55d1e78a5223ca0
SamarthBagga <samarthbagga@gmail.com>
2024-02-26 21:25:20 +0530

    2

--- blockchain/contracts/SupplyContract2.sol
+++ blockchain/contracts/SupplyContract2.sol
@@ -7,7 +7,7 @@
  * @dev idk
  */
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.22;
 
 interface IERC20 {
     function balanceOf(address account) external view returns (uint);
@@ -15,26 +15,34 @@ interface IERC20 {
 
 contract SupplyContract2 {
     address private immutable i_owner;
-    Order[] public activeOrders;
-    Order[] public pastOrders;
+
     struct Node {
+        uint256 nodeId;
         string name;
-        string typeOfNode; // whether it is a manufacturer supplier etc
+        string typeOfNode; 
         address add;
-        uint256[] ordersPresent; // only the id of the orders, if we want the orders present at the node then we would have to loop through the activeOrders and match the id from this array to get the orders present at the location
+        uint256[] ordersPresent;
         string location;
-        uint256 profitShare; // Must be in points as the cost of the order will be divided
-        // uint256 payment;
+        uint256 profitShare;
+        address nodeAddress;
     }
+
     struct Order {
         string name;
-        uint256 id;
+        uint256 orderId;
         string currLocation;
         string finalLocation;
-        Node[] pathTillNow;
+        Node[] pathTillNow; 
         uint256 cost;
         bool isDone;
     }
+
+    Order[] public activeOrders;
+    Order[] public pastOrders;
+    mapping(uint256 => Node) public nodesArr;
+    uint256 nodesArrayLen = 0;
+
+    mapping(uint256 => Node) public nodeMapping; // Mapping to store Node information
 
     constructor() {
         i_owner = msg.sender;
@@ -43,4 +51,93 @@ contract SupplyContract2 {
     function getActiveOrders() public view returns (Order[] memory) {
         return activeOrders;
     }
+    
+    function getNodes() public view returns (Node[] memory) {
+    uint256 nodeCount = 0; // Initialize a counter for nodes
+
+    // Iterate through the mapping to count the number of nodes
+    for (uint256 i = 0; ; i++) {
+        if (nodesArr[i].nodeId == 0) {
+            break; // Stop iterating when you encounter an empty node (default value)
+        }
+        nodeCount++;
+    }
+
+    Node[] memory nodes = new Node[](nodeCount); // Create an empty array
+
+    // Iterate again to copy nodes from the mapping to the array
+    for (uint256 i = 0; i < nodeCount; i++) {
+        nodes[i] = nodesArr[i];
+    }
+
+    return nodes;
+}
+
+
+
+    function getActiveOrder(uint256 orderIdP) public view returns (Order memory) {
+        for (uint i = 0; i < activeOrders.length; i++) {
+            if (activeOrders[i].orderId == orderIdP) {
+                return activeOrders[i];
+            }
+        }
+    }
+
+    function getPastOrder(uint256 orderIdP) public view returns (Order memory) {
+        for (uint i = 0; i < pastOrders.length; i++) {
+            if (pastOrders[i].orderId == orderIdP) {
+                return pastOrders[i];
+            }
+        }
+    }
+
+    function createNode(
+        uint256 _id,
+        string memory _name,
+        string memory _typeOfNode,
+        address _add,
+        uint256[] memory _ordersPresent,
+        string memory _location,
+        uint256 _profitShare,
+        address _nodeAddress
+    ) external {
+        Node memory newNode = Node({
+            nodeId: _id,
+            name: _name,
+            typeOfNode: _typeOfNode,
+            add: _add,
+            ordersPresent: _ordersPresent,
+            location: _location,
+            profitShare: _profitShare,
+            nodeAddress: _nodeAddress
+        });
+        nodesArrayLen++;
+        nodesArr[nodesArrayLen] = newNode;
+        nodeMapping[_id] = newNode; // Store Node information in mapping
+    }
+
+    function createOrder(
+    string memory _name,
+    uint256 _id,
+    string memory _currLocation,
+    string memory _finalLocation,
+    uint256 _cost,
+    bool _isDone
+) external {
+    Node[] memory array = new Node[](nodesArrayLen); // Create an array for the starting node
+    array[0] = nodesArr[0]; // Assign the first node
+
+    Order memory newOrder = Order({
+        name: _name,
+        orderId: _id,
+        currLocation: _currLocation,
+        finalLocation: _finalLocation,
+        pathTillNow: array,
+        cost: _cost,
+        isDone: _isDone
+    });
+
+    activeOrders.push(newOrder);
+}
+
 }
