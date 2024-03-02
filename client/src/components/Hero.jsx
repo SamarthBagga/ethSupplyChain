984ad801c9ffc38e301fae3b2f7b460e0139d830
Rahul Arora <rahularora2715@gmail.com>
2024-03-02 23:53:25 +0530

    did stuff

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import truck from '../assets/truck.webp';
 
 const Hero = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col justify-center items-center max-w-2xl h-60 p-6 bg-cyan-100 text-center border-4 border-slate-800">
-        <h2 className="text-5xl font-bold mb-10 text-black">Welcome to Our Website</h2>
-        <div className="flex space-x-4">
+    <div className="hero min-h-screen bg-cover bg-center flex items-center justify-center">
+      <img src={truck} alt="Hero background" className="absolute inset-0 object-cover w-full h-full opacity-60" />
+      <div className="container mx-auto px-4 py-16 text-center z-10">
+        <h1 className="text-5xl font-bold text-black mb-8">Welcome to the Website</h1>
+        <div className="flex justify-center space-x-4">
           <Link to="/register">
-            <button className="px-6 py-3 bg-teal-500 hover:bg-teal-600 rounded-md text-lg font-semibold text-gray-100">
+            <button className="py-3 w-40 bg-teal-500 hover:bg-teal-600 rounded-md text-lg font-semibold text-gray-100">
               Register
             </button>
           </Link>
           <Link to="/login">
-            <button className="px-6 py-3 bg-blue-500 hover:bg-blue-600 rounded-md text-lg font-semibold text-gray-100">
+            <button className="py-3 w-40 bg-blue-500 hover:bg-blue-600 rounded-md text-lg font-semibold text-gray-100">
               Login
             </button>
           </Link>
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import scm from '../assets/scm.png';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -34,37 +35,36 @@ function Login() {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="max-w-md mx-auto my-10 p-6 bg-cyan-100 border-4 border-slate-800">
-        <h1 className="text-2xl font-bold mb-4 text-black text-center">Login</h1>
+    <div className="flex min-h-screen">
+      <div className="w-3/4 hidden md:flex bg-gray-100 justify-center items-center">
+        <img src={scm} alt="Login Image" className="w-full h-full object-cover" />
+      </div>
+      <div className="flex flex-col justify-center w-full md:w-1/3 px-8 space-y-8 bg-white">
+        <h1 className="text-5xl font-bold text-gray-800 text-center">Login</h1>
         <form onSubmit={loginUser} className="space-y-4">
           <input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Email"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
-          <br />
-
           <div className="flex relative">
             <input
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type={isVisible ? 'text' : 'password'}
               placeholder="Password"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
             <button
               type="button"
-              className="absolute right-2 top-3 text-teal-500 hover:text-teal-600"
+              className="absolute right-2 top-3 text-blue-500 hover:text-blue-600"
               onClick={() => setIsVisible(!isVisible)}
             >
               {isVisible ? <FaEye /> : <FaEyeSlash />}
             </button>
           </div>
-
-          <br />
           <input
             type="submit"
             value="Login"
